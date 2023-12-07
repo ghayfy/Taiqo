2324eb6422ecede6320a0e913f580824398d1caa
ghayfy <75206538+ghayfy@users.noreply.github.com>
2023-12-07 13:43:20 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,35 @@ document.addEventListener('DOMContentLoaded', function () {
         navLinks.classList.toggle('show');
     });
 });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    adjustTitleSizes(); // Appel initial pour ajuster les tailles au chargement de la page
+  });
+
+  // Fonction pour ajuster les tailles des titres
+  function adjustTitleSizes() {
+    const titles = document.querySelectorAll(".block h2");
+    let maxHeight = 0;
+
+    // Réinitialisez la hauteur pour les titres
+    titles.forEach((title) => {
+      title.style.height = "auto";
+    });
+
+    // Trouvez la hauteur maximale parmi les titres
+    titles.forEach((title) => {
+      maxHeight = Math.max(maxHeight, title.offsetHeight);
+    });
+
+    // Appliquez la hauteur maximale à tous les titres
+    titles.forEach((title) => {
+      title.style.height = `${maxHeight}px`;
+      title.style.backgroundColor = "white";
+    });
+  }
+
+  // Réajuster la taille des titres lors du redimensionnement de la fenêtre
+  window.addEventListener("resize", function () {
+    adjustTitleSizes();
+  });
+
